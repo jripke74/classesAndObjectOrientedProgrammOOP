8283c305b880f278e8a82f7335848f8484e83a4b
jripke74 <jripke74@gmail.com>
2020-05-12 11:10:16 -0700

    add product list and html

---
+++ assets/styles/scripts/app.js
@@ -0,0 +1,40 @@
+const productList = {
+  products: [
+    {
+      title: 'A Pillow',
+      imageUrl: '../images/IMG_1003.jpeg',
+      price: 19.99,
+      description: 'A soft pillow!'
+    },
+    {
+      title: 'A Pillow',
+      imageUrl: '../images/IMG_1009.jpeg',
+      price: 89.99,
+      description: 'A carpet which you might like - or not.'
+    }
+  ],
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const prodEl = document.createElement('li');
+      prodEl.className = 'product-item';
+      prodEl.innerHTML = `
+        <div>
+          <img src="${prod.imageUrl}" alt="${prod.title}">
+            <div class="product-item__content">
+            <h2>${prod.title}</h2>
+            <h3>${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+};
+
+productList.render();
